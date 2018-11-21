85e6b64158ffe6867a95f1284496811e28d5ae33
Leo Bunyea <lrbunyea@wpi.edu>
2018-11-21 14:34:21 -0500

    Added player body and vertical bounce
    Happy with values and feel, next is to add key combinations that result in the bounce.

---
+++ FriendlyFriends/Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour {
+
+    #region Variables
+    //Singleton pattern
+    public static InputManager Instance;
+
+    //Keypress events
+    public UnityEvent SuccessfulFlap;
+    #endregion
+
+    #region Unity API Functions
+    void Awake () {
+        //Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        //Initalize key press events here
+        SuccessfulFlap = new UnityEvent();
+    }
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SuccessfulFlap.Invoke();
+        }
+	}
+    #endregion
+}
---
+++ FriendlyFriends/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Variables
+    //Physics values
+    [SerializeField] float flapStrength;
+
+    private Rigidbody playerBody;
+    #endregion
+
+    #region Unity API Functions
+    void Start () {
+        //Search for relevant components
+        playerBody = this.GetComponent<Rigidbody>();
+        //Subscribe funcitons to events
+        InputManager.Instance.SuccessfulFlap.AddListener(Flap);
+	}
+    #endregion
+
+    #region Movement Functions
+    /// <summary>
+    /// Function that is invoked when the user sucessfully inputs keys to flap
+    /// Propels the character model upwards
+    /// </summary>
+    private void Flap()
+    {
+        playerBody.AddForce(new Vector3(0, flapStrength, 0), ForceMode.Acceleration);
+    }
+    #endregion
+}
