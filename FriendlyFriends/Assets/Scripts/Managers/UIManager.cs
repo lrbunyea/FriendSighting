35dd497a50fb640d4c85899869cedbb057b22af5
Leo Bunyea <lrbunyea@wpi.edu>
2019-01-24 18:30:23 -0500

    UImanager and Gamemanager skeletons implemented

---
+++ FriendlyFriends/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    #region Variables
+    //Singleton pattern
+    public static UIManager Instance;
+    #endregion
+
+    #region Unity API Functions
+    void Awake()
+    {
+        //Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region UI Element Functions
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene("ControlTest1");
+    }
+    #endregion
+}
