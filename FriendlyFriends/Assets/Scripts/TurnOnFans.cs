e327d13e97bfb858a2b95093f35980dfa6616453
Unknown <jacattelona@wpi.edu>
2020-02-24 17:06:33 -0500

    Steam Sounds + Music

--- FriendlyFriends/Assets/Scripts/TurnOnFans.cs
+++ FriendlyFriends/Assets/Scripts/TurnOnFans.cs
@@ -6,12 +6,30 @@ public class TurnOnFans : MonoBehaviour
 {
     public CeilingFan[] fans;
     public ParticleSystem part;
+    public AudioClip fanStart;
+    public AudioClip fanLoop;
+    AudioSource[] auds;
+    bool fanStarted = false;
+    void Start()
+    {
+        auds = GetComponents<AudioSource>();
+        auds[0].loop = false;
+        auds[1].loop = true;
+    }
 
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            if (!fanStarted)
+            {
+                auds[0].PlayOneShot(fanStart);
+                auds[1].clip = fanLoop;
+                auds[1].PlayDelayed(4.0f);
+                fanStarted = true;
+            }
+            
             //turn on all the fans
             foreach (CeilingFan f in fans)
             {
